5a10f0334fce6adb4a0cf4c904fe019a5128a72c
maaz <maazmachkal@gmail.com>
2021-11-01 16:56:26 +0530

    push

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // creating variables for our
+    // widgets in xml file.
+    private RecyclerView chatsRV;
+    private ImageButton sendMsgIB;
+    private EditText userMsgEdt;
+    private final String USER_KEY = "user";
+    private final String BOT_KEY = "bot";
+
+    // creating a variable for
+    // our volley request queue.
+    private RequestQueue mRequestQueue;
+
+    // creating a variable for array list and adapter class.
+    private ArrayList<MessageModal> messageModalArrayList;
+    private MessageRVAdapter messageRVAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // on below line we are initializing all our views.
+        chatsRV = findViewById(R.id.idRVChats);
+        sendMsgIB = findViewById(R.id.idIBSend);
+        userMsgEdt = findViewById(R.id.idEdtMessage);
+
+        // below line is to initialize our request queue.
+        mRequestQueue = Volley.newRequestQueue(MainActivity.this);
+        mRequestQueue.getCache().clear();
+
+        // creating a new array list
+        messageModalArrayList = new ArrayList<>();
+
+        // adding on click listener for send message button.
+        sendMsgIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // checking if the message entered
+                // by user is empty or not.
+                if (userMsgEdt.getText().toString().isEmpty()) {
+                    // if the edit text is empty display a toast message.
+                    Toast.makeText(MainActivity.this, "Please enter your message..", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // calling a method to send message
+                // to our bot to get response.
+                sendMessage(userMsgEdt.getText().toString());
+
+                // below line we are setting text in our edit text as empty
+                userMsgEdt.setText("");
+            }
+        });
+
+        // on below line we are initialing our adapter class and passing our array lit to it.
+        messageRVAdapter = new MessageRVAdapter(messageModalArrayList, this);
+
+        // below line we are creating a variable for our linear layout manager.
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this, RecyclerView.VERTICAL, false);
+
+        // below line is to set layout
+        // manager to our recycler view.
+        chatsRV.setLayoutManager(linearLayoutManager);
+
+        // below line we are setting
+        // adapter to our recycler view.
+        chatsRV.setAdapter(messageRVAdapter);
+    }
+
+    private void sendMessage(String userMsg) {
+        // below line is to pass message to our
+        // array list which is entered by the user.
+        messageModalArrayList.add(new MessageModal(userMsg, USER_KEY));
+        messageRVAdapter.notifyDataSetChanged();
+        switch (userMsg){
+            case "hi":
+                messageModalArrayList.add(new MessageModal("Hi, my friend, what would you like to talk about today?", BOT_KEY));
+                messageRVAdapter.notifyDataSetChanged();
+                break;
+            case "hello":
+                messageModalArrayList.add(new MessageModal("Hi, my friend! Do you want me to tell you a joke?", BOT_KEY));
+                messageRVAdapter.notifyDataSetChanged();
+                break;
+            case "how are you":
+                messageModalArrayList.add(new MessageModal("I'm fine and excited to talk with you. How are you?", BOT_KEY));
+                messageRVAdapter.notifyDataSetChanged();
+                break;
+            default:
+                messageModalArrayList.add(new MessageModal("what ?", BOT_KEY));
+                messageRVAdapter.notifyDataSetChanged();
+                break;
+        }
+
+        // url for our brain
+        // make sure to add mshape for uid.
+        // make sure to add your url.
+        String url = "http://api.brainshop.ai/get?bid=160793&key=Nl75g1os4bTciBQ1&uid=[uid]&msg=" + userMsg;
+
+        // creating a variable for our request queue.
+
+    }
+}
